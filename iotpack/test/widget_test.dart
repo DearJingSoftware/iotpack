a4e230b32a7b38337f21e043d9ba12803194a576
jingdor <945627077@qq.com>
2020-05-28 15:41:00 +0800

    fixflutter test

--- iotpack/test/widget_test.dart
+++ iotpack/test/widget_test.dart
@@ -6,7 +6,7 @@ import 'package:iotpack/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(IotPack());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
