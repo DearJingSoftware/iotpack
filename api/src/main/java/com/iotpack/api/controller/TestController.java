7e283e106eb1443a44cf4dba07fde4901c91a0fa
jingdor <945627077@qq.com>
2020-06-01 16:39:32 +0800

    '内建mqttbroker测试'

--- api/src/main/java/com/iotpack/api/controller/TestController.java
+++ api/src/main/java/com/iotpack/api/controller/TestController.java
@@ -56,35 +56,25 @@ public class TestController extends BaseController {
 
 
     @RequestMapping("/startmqtt")
-    public ResDto startMqtt() throws IOException, InterruptedException {
+    public ResDto startMqtt() throws IOException {
         IResourceLoader classpathLoader = new ClasspathResourceLoader();
+
         final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
 
         final Server mqttBroker = new Server();
+
         List<? extends InterceptHandler> userHandlers = Collections.singletonList(new PublisherListener());
+
         mqttBroker.startServer(classPathConfig, userHandlers);
 
         System.out.println("Broker started press [CTRL+C] to stop");
-        //Bind  a shutdown hook
+
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Stopping broker");
             mqttBroker.stopServer();
             System.out.println("Broker stopped");
         }));
 
-//        Thread.sleep(20000);
-//        System.out.println("Before self publish");
-//        MqttPublishMessage message = MqttMessageBuilders.publish()
-//                .topicName("/exit")
-//                .retained(true)
-////        qos(MqttQoS.AT_MOST_ONCE);
-////        qQos(MqttQoS.AT_LEAST_ONCE);
-//                .qos(MqttQoS.EXACTLY_ONCE)
-//                .payload(Unpooled.copiedBuffer("Hello World!!".getBytes(UTF_8)))
-//                .build();
-//
-//        mqttBroker.internalPublish(message, "INTRLPUB");
-//        System.out.println("After self publish");
         return success("success");
     }
 
