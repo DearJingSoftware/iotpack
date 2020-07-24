939d9b9f5ea372ccd7a82ebaa16bd0cd3c46db01
jingdor <945627077@qq.com>
2020-07-24 19:16:43 +0800

    '更新ui显示'

---
+++ web/src/uikit/UIPlaceholder.svelte
@@ -0,0 +1,6 @@
+<script>
+  export var name='';
+</script>
+<div>
+  {name}
+</div>
---
+++ web/src/uikit/UIProgress.svelte
@@ -0,0 +1,22 @@
+<script>
+  export var name='';
+</script>
+
+<style>
+ .progress {
+   height:6px;
+   border-radius: 1px;
+  background: #f2f4f6;
+  display: flex;
+  justify-content:start;
+  justify-items: center;
+}
+ .number {
+   width: 50%;
+   background-color: lightgreen;
+ }
+</style>
+<div class="progress">
+  <div class="number">
+  </div>
+</div>
