dc288f70448ad45659ac955e5e2daa5980f04def
jingdor <945627077@qq.com>
2020-07-28 10:31:43 +0800

    '更新grid'

---
+++ web/src/uikit/Avatar.svelte
@@ -0,0 +1,43 @@
+<script>
+export let size='middle';
+export let src='';
+//circle
+export let style='circle';
+</script>
+<style>
+  .big {
+    width: 128px;
+    height: 128px;
+  }
+  
+  .middle {
+    width: 64px;
+    height: 64px;
+  }
+  
+  .small {
+    width: 32px;
+    height: 32px;
+  }
+
+  .square {
+
+  }
+  
+  .round {
+    border-radius: 4px;
+  }
+  
+  .circle {
+    border: 50%;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
+<div class="{size} {style}" >
+  <img {src} alt="avatar"/>
+</div>
