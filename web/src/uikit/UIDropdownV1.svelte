a05eecae39445f5a1a83c963d16cc951dc4679ad
jingdor <945627077@qq.com>
2020-07-31 19:21:51 +0800

    '更新'

---
+++ web/src/uikit/UIDropdownV1.svelte
@@ -0,0 +1,50 @@
+
+<script>
+export let status='default';
+export let active='';
+import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  
+  function handleClick() {
+		dispatch('click', {});
+	}
+</script>
+
+
+<style>
+.dropdown {
+  position:relative;
+}
+.dropdown:hover .menu_warp{
+  display: block;
+}
+.dropdown:hover .menu_warp:hover {
+  display: block;
+}
+.menu_warp {
+  display: none;
+  z-index: 200;
+  top:16px;
+  position:absolute;
+  width: 80px;
+  left: -64px;
+  box-shadow: 0px 2px 16px rgba(153,155,168,0.12);
+  margin: 0px;
+  padding: 0px;
+  border: 2px solid rgba(153,155,168,0.12);
+  background-color: white;
+}
+
+</style>
+
+<div class="dropdown">
+  <slot>
+  
+  </slot>
+  <div class="menu_warp">
+    <slot name="menu">
+    </slot>
+  </div>
+
+</div>
