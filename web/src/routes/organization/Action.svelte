b22e41d5e174320087b9af60a6295e71f9e20b87
jingdor <945627077@qq.com>
2020-08-11 15:41:40 +0800

    '更新table组件'

---
+++ web/src/routes/organization/Action.svelte
@@ -0,0 +1,11 @@
+<style>
+
+</style>
+<div>
+<i class="las la-arrow-up"></i>
+<i class="las la-arrow-down"></i>
+<i class="las la-arrow-left"></i>
+<i class="las la-arrow-right"></i>
+<i class="las la-edit"></i>
+<i class="las la-trash"></i>
+</div>
---
+++ web/src/uikit/UIOnline.svelte
@@ -0,0 +1,13 @@
+<script>
+export let is_online={};
+</script>
+
+<style>
+.online{
+  color: green;
+}
+.offline{
+  color: red;
+}
+</style>
+<i class=" {is_online?'fas fa-circle online':'lar la-circle offline'}"></i>
